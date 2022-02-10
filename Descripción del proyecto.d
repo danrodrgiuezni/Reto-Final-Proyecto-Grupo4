b0556b6e6cc064a4e1f0041221b7ff10278b941f
danrodrgiuezni <99033363+danrodrgiuezni@users.noreply.github.com>
2022-02-10 16:49:40 -0500

    Update and rename README.md to Descripción del proyecto.d

---
+++ Descripción del proyecto.d
@@ -0,0 +1,6 @@
+Código del proyecto final realizado por el grupo 4, conformado por: Daniela Alejandra Rodriguez Nieto, Edwin Hernan Rojas Neita, Duván Estiven Valencia Velandia, Lina María Vela Muñoz y Jhorman Enrique Osorio Aguirre.
+
+Este proyecto consiste en presentar al usuario la opción de jugar 2 juegos muy conocidos, el triqui, con posibilidad para uno o dos jugadores y el ahorcado con 10 palabras referentes a la programación. Adicionalmente y como razón principal de nuestro proyecto es la unión de estos dos juegos mediante un codigo simple, algo sencillo pero divertido, lo llamamos el triquiahorcado.
+
+Los temas vistos en clase que aplicamos a nuestro código fueron: Funciones, Condicionales, Ciclos for y while, Cadenas, Listas, Matrices, JSON y Librerias.
+Esperamos les guste.
--- README.md
+++
@@ -1,2 +0,0 @@
-# Reto-Final-Proyecto-Grupo4
-Código del proyecto final realizado por el grupo 4, conformado por: Daniela Alejandra Rodriguez Nieto, Edwin Hernan Rojas Neita, Duván Estiven Valencia Velandia, Lina María Vela Muñoz y Jhorman Enrique Osorio Aguirre
