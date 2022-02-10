fc2f1627c56695b57aec0ff501a3787e486c40f2
danrodrgiuezni <99033363+danrodrgiuezni@users.noreply.github.com>
2022-02-10 17:23:39 -0500

    Update README.d

--- README.d
+++ README.d
@@ -1,6 +1,32 @@
-Código del proyecto final realizado por el grupo 4, conformado por: Daniela Alejandra Rodriguez Nieto, Edwin Hernan Rojas Neita, Duván Estiven Valencia Velandia, Lina María Vela Muñoz y Jhorman Enrique Osorio Aguirre.
+Código del proyecto final realizado por el grupo 4,conformado por: Daniela Alejandra Rodriguez Nieto, 
+Edwin Hernan Rojas Neita, Duván Estiven Valencia Velandia, Lina María Vela Muñoz y Jhorman Enrique 
+Osorio Aguirre.
 
-Este proyecto consiste en presentar al usuario la opción de jugar 2 juegos muy conocidos, el triqui, con posibilidad para uno o dos jugadores y el ahorcado con 10 palabras referentes a la programación. Adicionalmente y como razón principal de nuestro proyecto es la unión de estos dos juegos mediante un codigo simple, algo sencillo pero divertido, lo llamamos el triquiahorcado.
+Este proyecto consiste en presentar al usuario la opción de jugar 2 juegos muy conocidos, el triqui, 
+con posibilidad para uno o dos jugadores y el ahorcado con 10 palabras referentes a la programación. 
+Adicionalmente y como razón principal de nuestro proyecto es la unión de estos dos juegos mediante un
+codigo simple, algo sencillo pero divertido, lo llamamos el triquiahorcado.
 
-Los temas vistos en clase que aplicamos a nuestro código fueron: Funciones, Condicionales, Ciclos for y while, Cadenas, Listas, Matrices, JSON y Librerias.
+Los temas vistos en clase que aplicamos a nuestro código fueron: Funciones, Condicionales, Ciclos for 
+y while, Cadenas, Listas, Matrices, JSON y Librerias.
+
+Las referencias utilizadas fueron:
+
+-Python: 3 En Raya #1 - YouTube. Visitado enero 12, 2022, de
+ https://www.youtube.com/watch?v=6M8MBT7La68
+
+-Python - Nivel 15 - Juego del ahorcado - YouTube. Visitado enero
+ 23, 2022, de https://www.youtube.com/watch?v=gPNis50DGwo
+
+-Aprende a usar funciones en Python. Curso Python para Análisis de
+ datos. YouTube. Visitado Enero 15, 2022, de https://www.youtube.com/watch?v=w0LqU99RRy8
+
+-Python - Volver a ejecutar una parte del codigo hasta que se cumpla- 
+ Stack Overflow en español (n.d.). Visitado febrero 2, 2022, de 
+ https://es.stackoverflow.com/questions/67977/volver-a-ejecutar-una-parte-del-codigo-hasta-que-se-cumpla
+
+-Python Inteligencia Artificial - Tres en Raya / Tic Tac Toe - YouTube.
+ Visitado Enero 30, 2022, de https://www.youtube.com/watch?v=YAbz0plIDe4&#38;t=36s
+
+
 Esperamos les guste.
